1515a92a9fa51f540b5d8e0f0986fd77b4a4b22a
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-07-19 02:46:07 -0400

    Guns

---
+++ src/StarterPlayer/StarterPlayerScripts/Client/Main/Item Classes/init.lua
@@ -0,0 +1,4 @@
+--// Main class for viewmodel...
+local REPLICATEDSTORAGE = game:GetService("ReplicatedStorage")
+local VIEWMODEL = REPLICATEDSTORAGE.Master:WaitForChild("Viewmodel")
+local Class = {}
